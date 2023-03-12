122fa585c51e606c6453e91672b8285b6150aab0
FIRAT ATALAY <atafiratbey@gmail.com>
2023-03-12 17:15:27 +0300

    PROMISES AND FETCH API..

---
+++ IntermediateJs/Asynchronous JavaScript, AJAX and APIs/consumingPromises and FETCH-API/script.js
@@ -0,0 +1,56 @@
+'use strict';
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+///////////////////////////////////////////////
+// Consuming Promises
+///////////////////////////////////////////////
+
+//********        IMPORTANT   ***********************
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      renderCountry(data[0]);
+    });
+};
+
+getCountryData('portugal');
+
+///////////////////////////////////////
--- IntermediateJs/Asynchronous JavaScript, AJAX and APIs/promisesAndFetc-APIs/script.js
+++ IntermediateJs/Asynchronous JavaScript, AJAX and APIs/promisesAndFetc-APIs/script.js
@@ -21,16 +21,11 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  countriesContainer.style.opacity = 1;
-};
+///////////////////////////////////////////////
+// consumingPromises and FETCH-API
+///////////////////////////////////////////////
 
-///////////////////////////////////////
-// Consuming Promises
-// Chaining Promises
-// Handling Rejected Promises
-// Throwing Errors Manually
+//********  Normal expr-function kullanark fetch API'dan veri alabilirim   ***********************
 
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
@@ -44,44 +39,59 @@ const renderError = function (msg) {
 //     });
 // };
 
+//FAKAT ARROW FUNCTION KULLANMAK BURDA GOZE DAHA COK HITAP EDIYOR...
+
 const getCountryData = function (country) {
   // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => {
       console.log(response);
-
-      if (!response.ok)
-        throw new Error(`Country not found (${response.status})`);
-
       return response.json();
     })
     .then(data => {
+      console.log(data);
       renderCountry(data[0]);
-      // const neighbour = data[0].borders[0];
-      const neighbour = 'dfsdfdef';
-
-      if (!neighbour) return;
-
-      // Country 2
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-    })
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`Country not found (${response.status})`);
-
-      return response.json();
-    })
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      console.error(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
     });
 };
 
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
+getCountryData('portugal');
+
+/*****OZET****
+Once "fetch" ile Online API'dan verinin asenkron bicimde alinmasini baslattim.
+------> fetch(`https://restcountries.com/v3.1/name/${country}`)
+
+//
+Bu fetch metodu asenkron olarak verimi yavas yavas API'dan alimimi gerceklestirirkene cikti olarak
+baslangicta bana bir "PROMISE" dondurecek.Ve bu PROMISE'imin bir Obje oldugunu ve daima en basta default olara "PENDING" yani bekleme aninda oldugunu bilmelisin. Arka planda bu promise'imin pending durumu yavas yavas ortadan kalkiyor cunku istedigim veriyi arka planda yavas yavas elde ediyorum.
+Sonrasinda benim bu promise'im pending durumundan kendinisini cikardiktan sonra "SETTLED" durumuna getirecek. BU DURUMDA son olrak promisem veri almayi basarili sekilde asenkron olarak tamamlarsa "FULLFILLED" , basarisiz olarak tamamlarsa "REJECTED" olucak.  
+
+////
+Ben promisemin pending durumunda veriyi arka planda almaya devam ederken, SETTLED haline geldiginde veri aliminin basarili oldugunu kabul edip almak istedigim veriyi konsolda goruntuleyebilmek icin ya da manipule edebilmek icin "then" metodunu kullanicam.Bu "then" metodum basarili
+parametre aliyor. 
+ 1.PARAMETREM, SETTLED DURUMU FULFILLED OLURSA NE YAPILSIN?
+ 2.PARAMETREM SETTLED DURUMU REJECTED OLURSA NE YAPILSIN?
+
+ -------->   fetch(`https://restcountries.com/v3.1/name/${country}`).then(response => {
+      console.log(response);
+    }
+
+Yukardaki kodumda FULFILLED durumunda alinan data neyse yani responsem onu konsola yazdir,dedim.
+Fakat seninde fark ettigin gibi bu response benim isime yarayamiyor cunku bu konsoldaki ciktinin body stream kisminda ulkenin bilgileri yer aliyor ve burda kilitli benim asil amacim  bu verileri elde etmekti,Peki bunu nasil yapicam?
+Asil istedigim bu veriyi elde edebilmek icin bu response'mu "JSON" metodumla cagirmaliyim.
+JSON methodum arka planda 2.bir asenkron cagrisi gerceklestirecek.Yani ikinci bir promise tetikleyecek.Ben iste o 2. promisimin data'sini istiyorum.
+Buyuzden bu sekilde then metoduyla promisleri birbirine baglamam gerekti.
+
+
+//----->  
+fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);   //ASIL ISTEDIGIM DATA BUYDU:>//
+    });
+};
+*/
+
 ///////////////////////////////////////
